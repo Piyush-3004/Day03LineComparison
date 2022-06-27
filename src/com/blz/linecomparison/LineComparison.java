8f5edd454aeee047601c447833204951c355cb21
Chetan Patil <piyushp0541@gmail.com>
2022-06-27 22:35:30 +0530

    added uc1

--- src/com/blz/linecomparison/LineComparison.java
+++ src/com/blz/linecomparison/LineComparison.java
@@ -5,6 +5,15 @@ public class LineComparison {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Line Comparison Computation Programs");
+
+//      point coordinates		
+		long x1=0,x2=0,y1=0,y2=0;
+
+        double len1;
+	
+        len1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+	    
+        System.out.println("Length of line is : "+len1);
 	}
 
 }
